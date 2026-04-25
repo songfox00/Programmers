8866d777400ad32717a86f030ac0836e6fbf9f87
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2026-04-25 16:08:57 +0900

    Create 공배수.kt

---
+++ Coding Test/공배수.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(number: Int, n: Int, m: Int): Int {
+        var answer: Int = 0
+        
+        if(number%n==0 && number%m==0)
+            answer=1
+        
+        return answer
+    }
+}
