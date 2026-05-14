75bcdb6ab2df3e246ef80dc5181eb42f65a196a9
songfox00 <songfox00@gmail.com>
2026-05-14 22:35:12 +0900

    rny_string

---
+++ Coding Test/rny_string.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(rny_string: String): String {
+        var answer: String = ""
+        
+        answer=rny_string.replace("m","rn")
+        
+        return answer
+    }
+}
