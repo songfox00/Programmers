f7b782ac14cd1e1cecdda22b3e536de0f5ba0372
songfox00 <songfox00@gmail.com>
2026-05-19 23:31:59 +0900

    문자열 섞기

---
+++ Coding Test/문자열 섞기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(str1: String, str2: String): String {
+        var answer: String = ""
+        
+        for(i in 0 until str1.length){
+            answer+=str1[i].toString()+str2[i].toString()
+        }
+        
+        return answer
+    }
+}
