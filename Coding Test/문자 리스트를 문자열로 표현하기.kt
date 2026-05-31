5efe46dbd448fed7cc5620a49410def93b0b532f
songfox00 <songfox00@gmail.com>
2026-05-31 23:58:57 +0900

    문자 리스트를 문자열로 표현하기

---
+++ Coding Test/문자 리스트를 문자열로 표현하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(arr: Array<String>): String {
+        var answer: String = ""
+        
+        for(c in arr){
+            answer+=c
+        }
+        
+        return answer
+    }
+}
